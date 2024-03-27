3e882593b4be9676b3075a751409c2d3b5d316fa
stas9132 <ssgolobokov@gmail.com>
2024-03-27 14:13:57 +0300

    upd config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"encoding/hex"
 	"flag"
+	"log"
+	"os"
 )
 
 var (
@@ -21,6 +23,38 @@ func Init() {
 	flag.Parse()
 
 	PrintVersion = *b
+
+	if s, ok := os.LookupEnv("LISTEN_ADDRESS"); ok {
+		ListenAddress = s
+	}
+	if s, ok := os.LookupEnv("LISTEN_ADDRESS_REST"); ok {
+		ListenAddressR = s
+	}
+	if s, ok := os.LookupEnv("S3_ENDPOINT"); ok {
+		S3Endpoint = s
+	}
+	if s, ok := os.LookupEnv("S3_ACCESS_KEY"); ok {
+		S3AccessKeyID = s
+	}
+	if s, ok := os.LookupEnv("S3_SECRET_KEY"); ok {
+		S3SecretAccessKey = s
+	}
+	if s, ok := os.LookupEnv("S3_USE_SSL"); ok {
+		S3UseSSL = s == "YES"
+	}
+	if s, ok := os.LookupEnv("S3_LOCATION"); ok {
+		S3Location = s
+	}
+	if s, ok := os.LookupEnv("AES_KEY"); ok {
+		AESKey = []byte(s)
+	}
+	if s, ok := os.LookupEnv("AES_NOONCE"); ok {
+		var err error
+		AESnonce, err = hex.DecodeString(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 var (
